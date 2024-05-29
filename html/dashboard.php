454736e6541640afe22a7cb97a55d7249bf073af
Khonn <159712094+Khonn@users.noreply.github.com>
2024-05-29 13:06:09 +0800

    Add files via upload

---
+++ html/dashboard.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+
+    <title>Insitify</title>
+    <link rel="icon" href="logo/symbol_blue.png">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/header.css">
+    
+</head>
+
+<body>
+    
+    <header>
+        <div class="logo_container">
+            <a href="">
+                <img class="logo_symbol" src="logo/symbol_blue.png" alt="symbol_logo">
+                <img class="logo_full" src="logo/full_blue.png" alt="logo_full">
+            </a>
+        </div>
+
+        <ul class="nav_list">
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Collection</a></li>
+            <li><a href="">Merch</a></li>
+        </ul>
+
+    </header>
+
+    <div class="content">
+        <h1 class="welcome">Welcome back, Chingu</h1>
+        <div class="stat_card">
+            <div class="container">
+                <div class="stat_container">
+                    <h2 class="desc">No. of Titles</h2>
+                    <h1 class="figure">--</h1>
+                </div>
+                <div class="stat_container">
+                    <h2 class="desc">Total Entries</h2>
+                    <h1 class="figure">--</h1>
+                </div>
+                <div class="stat_container">
+                    <h2 class="desc">Favorite Title</h2>
+                    <h1 class="figure">--</h1>
+                </div>
+            </div>
+            <img src="graphics/stats.png" alt="studying_girl">
+        </div>
+        <h1 class="feature">Features</h1>
+        <div class="feature_card">
+            <div class="feat_container">
+                <div class="feat_btn">
+                    <img class="icon" src="graphics/paraphrase1.png" alt="icon">
+                    <div>
+                        <h1 class="name">Paraphraser</h1>
+                        <h2 class="desc">rewrite sentences</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                <div class="feat_btn">
+                    <img class="icon" src="graphics/translate.png" alt="icon">
+                    <div>
+                        <h1 class="name">Translator</h1>
+                        <h2 class="desc">change language</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                <div class="feat_btn">
+                    <img class="icon" src="graphics/cite.png" alt="icon">
+                    <div>
+                        <h1 class="name">Cite Generator</h1>
+                        <h2 class="desc">cite sources</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                
+            </div>
+            <div class="feat_container">
+                <div class="feat_btn">
+                    <img class="icon" src="graphics/simplify.png" alt="icon">
+                    <div>
+                        <h1 class="name">Simplifier</h1>
+                        <h2 class="desc">rewrite sentences</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                <div class="feat_btn">
+                    <img class="icon" src="logo/symbol_blue.png" alt="icon">
+                    <div>
+                        <h1 class="name">Feature</h1>
+                        <h2 class="desc">description</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                <div class="feat_btn">
+                    <img class="icon" src="logo/symbol_blue.png" alt="icon">
+                    <div>
+                        <h1 class="name">Feature</h1>
+                        <h2 class="desc">description</h2> 
+                    </div>
+                    <img class="arrow" src="graphics/arrow.png" alt="arrow">
+                </div>
+                
+            </div>
+        </div>
+        <div class="other_card">
+            <div class="news_main">
+                <h1 class="news">News</h1>
+                <img src="graphics/ad.png" alt="ad">
+            </div>
+            <div class="recos_main">
+                <h1 class="reco">Recommendations</h1>
+                <img src="graphics/reco.png" alt="reco">
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+<style>
+
+    
+
+</style>
+
+</html>
---
+++ html/html_about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Insightify</title>
+    <link rel="icon" href="logo/symbol_blue.png">
+
+    <link rel="stylesheet" href="css/css_about.css">
+    
+</head>
+
+<body>
+
+    <div class="hero">
+        <h1>ABOUT US</h1>
+    </div>
+
+    <div class="title">
+        <!-- <h1>Who is</h1> -->
+        <img src="logo/symbol+full_blue2.png" alt="logo">
+    </div>  
+
+    <div class="content">
+        <p>In today's digital age, information surrounds us, but accessing and comprehending it 
+            can be challenging. At Insightify, we believe that everyone deserves equal access to 
+            knowledge and the ability to understand complex information efficiently. This belief 
+            fueled our creation of Insightify, a comprehensive text analysis and manipulation toolkit 
+            designed to empower your learning experience.
+        </p>
+        <h1>Our Mission</h1>
+        <div class="mission">
+
+            <p>Image Scanner: Unlock the information held within images. Our OCR (Optical Character 
+                Recognition) technology accurately scans text from pictures, making it editable and searchable.
+            </p>
+            <p>Citation Generator: Never stress over formatting references again! Our generator helps you 
+                easily create accurate citations in various styles, ensuring academic integrity.
+            </p>
+            <p>Text Translator: Language barriers are a thing of the past. Our translator bridges communication 
+                gaps, enabling you to understand and be understood across different languages.
+            </p>
+
+        </div>
+
+            <p>
+                Our team, composed of passionate developers, educators, and language enthusiasts, is dedicated 
+                to creating innovative tools that contribute to a more informed and connected world. We are inspired 
+                by the potential of technology to empower individuals of all backgrounds and learning styles to 
+            engage with and utilize information effectively.
+        </p>
+        <p>
+            We believe that Insightify is more than just an application; it's a gateway to unlocking the potential 
+            within text and empowering you to navigate the world of information with confidence and clarity.
+        </p>
+
+        <h1>The Insightify Team</h1>
+
+    </div>
+    <div class="members">
+        <div class="card">
+            <div class="img">
+                <img src="img/aris.jpg" alt="member">
+            </div>
+            <h1>Geraldine</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="img/dumalag.jpg" alt="member">
+            </div>
+            <h1>Iver</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="img/benson2.jpeg" alt="member">
+            </div>
+            <h1>Benson</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="img/madrunio.jpg" alt="member">
+            </div>
+            <h1>Samantha</h1>
+            <h2>Software Developer</h2>
+        </div>
+        <div class="card">
+            <div class="img">
+                <img src="img/ochavo.png" alt="member">
+            </div>
+            <h1>Redtide</h1>
+            <h2>Software Developer</h2>
+        </div>
+        
+    </div>
+    
+
+</body>
+
+<style>
+    
+    
+</style>
+
+</html>

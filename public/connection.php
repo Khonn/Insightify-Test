3edfdda0d58ac56d48e97c05c8a994bea5620d73
Khonn <159712094+Khonn@users.noreply.github.com>
2024-06-01 22:29:56 +0800

    Add files via upload

--- public/connection.php
+++ public/connection.php
@@ -1,11 +1,25 @@
 <?php
-$servername = "localhost";
-$username = "user";
-$password = "abc_123";
-$database = "mydatabase";
-$conn = mysqli_connect(
-$servername,
-$username,
-$password,
-$database);
+
+$servername = "sql6.freesqldatabase.com";
+$username = "sql6700012";
+$password = "D3gevjgE4F";
+$dbname = "sql6700012";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+function checkHashPassword($userPassword, $hash) {
+    return password_verify($userPassword, $hash);
+}
+
+
+
+$conn->close();
 ?>

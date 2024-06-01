1f64892ba9d140b6f577d23ceafac4ec4372e85c
Khonn <159712094+Khonn@users.noreply.github.com>
2024-06-02 01:00:11 +0800

    Add files via upload

--- public/login_function.php
+++ public/login_function.php
@@ -9,7 +9,7 @@ function getRandomString($length) {
     return bin2hex(random_bytes($length / 2));
 }
 
-function sha512($password, $salt) {
+function sha512($user_password, $salt) {
     $hash = hash_hmac('sha512', $password, $salt);
     return array(
         'salt' => $salt,
@@ -37,7 +37,7 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 		$data = trim($data);
 		$data = stripslashes($data);
 		$data = htmlspecialchars($data);
-		return;
+		return $data;
 	}
 }
 $email = validate($_POST['email']);
@@ -54,26 +54,18 @@ else if(empty($password)){
 
 $sql = "SELECT * FROM user_account WHERE email='$email'";
 
-$result = mysqli_query($conn, $sql);
+$resullt = $conn->query($sql);
 
-if(mysqli_num_rows($result) == 1){
+if($result->num_rows === 1){
 	$row = mysqli_fetch_assoc($result);
-	if($row['email'] == $email){
-	$salt = $row['salt'];
-	$encrypted_password = $row['password'];
-	$hashed_password = checkHashPassword($password, $salt)['passwordHash'];
-	if($encrypted_password == $hashed_password){
+	if($row['email'] === $email){
+	
 		echo "Logged In!";
 		$_SESSION['email'] = $row['email'];
 		$_SESSION['username'] = $row['username'];
 		header("Location: dashboard.php");
 		exit();
-	}
-	else{
-		echo "Incorrect Password";
-		exit();	
-	}
+	
+
 	}
 }
-
-?>

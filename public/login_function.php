251d0c9f799216245c7e861db9f800620cfc58a4
Khonn <159712094+Khonn@users.noreply.github.com>
2024-06-02 09:49:31 +0800

    Add files via upload

--- public/login_function.php
+++ public/login_function.php
@@ -8,7 +8,7 @@ $db_username = "sql6700012";
 $db_password = "D3gevjgE4F";
 $dbname = "sql6700012";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($db_servername, $db_username, $db_password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
